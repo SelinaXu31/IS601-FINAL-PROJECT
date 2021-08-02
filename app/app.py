8dd4ab1f12c710d008171ecf48b88f5c9db6c33d
Administrator <tx4@njit.edu>
2021-08-02 11:40:20 -0400

    Update app.py

--- app/app.py
+++ app/app.py
@@ -37,10 +37,16 @@ mysql.init_app(app)
 
 @app.route('/', methods=['GET'])
 def user_records():
-    """Create a user via query string parameters."""
     username = request.args.get('user')
     email = request.args.get('email')
     if username and email:
+        existing_user = User.query.filter(
+            User.username == username or User.email == email
+        ).first()
+        if existing_user:
+            return make_response(
+                f'{username} ({email}) already created!'
+            )
         new_user = User(
             username=username,
             email=email,
@@ -48,10 +54,15 @@ def user_records():
             bio="In West Philadelphia born and raised, \
             on the playground is where I spent most of my days",
             admin=False
-        )
+        )  # Create an instance of the User class
         db.session.add(new_user)  # Adds new User record to database
         db.session.commit()  # Commits all changes
-    return make_response(f"{new_user} successfully created!")
+        redirect(url_for('user_records'))
+    return render_template(
+        'users.Tianqi',
+        users=User.query.all(),
+        title="Show Users"
+    )
 
 @app.errorhandler(404)
 def not_found():
