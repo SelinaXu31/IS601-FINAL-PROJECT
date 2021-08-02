8d7bf64aa54d97b3e2ed781b49bb5dfbfaaed7d3
Administrator <tx4@njit.edu>
2021-08-01 22:05:03 -0400

    400, 404, & 500 error

--- app/app.py
+++ app/app.py
@@ -8,7 +8,7 @@ from flask import current_app as app
 from flask import request
 from flaskext.mysql import MySQL
 from pymysql.cursors import DictCursor
-from forms import ContactForm
+from forms import ContactForm, SignupForm
 
 app = Flask(__name__, template_folder="templates")
 app.config.from_object('config.Config')
@@ -21,15 +21,66 @@ app.config['MYSQL_DATABASE_PORT'] = 3306
 app.config['MYSQL_DATABASE_DB'] = 'oscarData'
 mysql.init_app(app)
 
-@app.route("/contact", methods=["GET", "POST"])
-def contact():
-    form = ContactForm()
-    if form.validate_on_submit():
-        return redirect(url_for("success"))
+@app.errorhandler(404)
+def not_found():
+    """Page not found."""
+    return make_response(
+        render_template("404.html"),
+        404
+     )
+
+
+@app.errorhandler(400)
+def bad_request():
+    """Bad request."""
+    return make_response(
+        render_template("400.html"),
+        400
+    )
+
+
+@app.errorhandler(500)
+def server_error():
+    """Internal server error."""
+    return make_response(
+        render_template("500.html"),
+        500
+    )
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup_page():
+    """User sign-up page."""
+    signup_form = SignupForm(request.form)
+    if request.method == 'POST':
+        if signup_form.validate():
+            name = request.form.get('name')
+            email = request.form.get('email')
+            password = request.form.get('password')
+            website = request.form.get('website')
+            existing_user = User.query.filter_by(email=email).first()
+            if existing_user is None:
+                user = User(
+                    name=name,
+                    email=email,
+                    password=generate_password_hash(
+                        password,
+                        method='sha256'
+                    ),
+                    website=website
+                )
+                db.session.add(user)
+                db.session.commit()
+                login_user(user)
+                return redirect(url_for('main_bp.dashboard'))
+            flash('A user exists with that email address.')
+            return redirect(url_for('auth_bp.signup_page'))
+    # GET: Serve Sign-up page
     return render_template(
-        "forms",
-        form=form,
-        template="form-template"
+        '/forms.html',
+        title='Create an Account | Flask-Login Tutorial.',
+        form=SignupForm(),
+        template='signup-page',
+        body="Sign up for a user account."
     )
 
 
