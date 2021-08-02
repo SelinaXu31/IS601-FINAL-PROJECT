8a2b5ecd35e9096913e6973a70044249c7e34e81
Administrator <tx4@njit.edu>
2021-08-02 11:55:55 -0400

    update forms.py

--- app/forms.py
+++ app/forms.py
@@ -5,53 +5,61 @@ from wtforms import (
     SubmitField,
     PasswordField,
     DateField,
-    SelectField
+    SelectField,
+    StringField,
+    PasswordField,
+    SubmitField
 )
 from wtforms.validators import (
     DataRequired,
     Email,
     EqualTo,
     Length,
-    URL
-)
+    URL,
 
 
 class SignupForm(FlaskForm):
+    name = StringField(
+        'Name',
+        validators=[DataRequired()]
+    )
     email = StringField(
         'Email',
-        [
-            Email(message='Not a valid email address.'),
+        validators=[
+            Length(min=6),
+            Email(message='Enter a valid email.'),
             DataRequired()
         ]
     )
     password = PasswordField(
         'Password',
-        [
-            DataRequired(message="Please enter a password."),
+        validators=[
+            DataRequired(),
+            Length(min=6, message='Select a stronger password.')
         ]
     )
-    confirmPassword = PasswordField(
-        'Repeat Password',
-        [
-            EqualTo(password, message='Passwords must match.')
-        ]
-    )
-    title = SelectField(
-        'Title',
-        [DataRequired()],
-        choices=[
-            ('Farmer', 'farmer'),
-            ('Corrupt Politician', 'politician'),
-            ('No-nonsense City Cop', 'cop'),
-            ('Professional Rocket League Player', 'rocket'),
-            ('Lonely Guy At A Diner', 'lonely'),
-            ('Pokemon Trainer', 'pokemon')
+    confirm = PasswordField(
+        'Confirm Your Password',
+        validators=[
+            DataRequired(),
+            EqualTo('password', message='Passwords must match.')
         ]
     )
     website = StringField(
         'Website',
-        validators=[URL()]
+        validators=[Optional()]
     )
-    birthday = DateField('Your Birthday')
-    recaptcha = RecaptchaField()
-    submit = SubmitField('Submit')
+    submit = SubmitField('Register')
+
+
+class LoginForm(FlaskForm):
+    """User Log-in Form."""
+    email = StringField(
+        'Email',
+        validators=[
+            DataRequired(),
+            Email(message='Enter a valid email.')
+        ]
+    )
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Log In')
