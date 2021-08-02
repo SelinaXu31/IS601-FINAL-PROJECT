29f1f24a86ad9a10d375f4f65cd82d77e4c755a3
Administrator <tx4@njit.edu>
2021-08-02 12:05:06 -0400

    update init.py

--- app/__init__.py
+++ app/__init__.py
@@ -14,8 +14,7 @@ def init_app():
     db.init_app(app)
 
     with app.app_context():
-        from . import routes  # Import routes
-        db.create_all()  # Create sql tables for our data models
+        db.create_all()
 
         return app
 
@@ -23,10 +22,8 @@ def init_app():
 def create_app():
     app = Flask(__name__, instance_relative_config=False)
 
-    # Application Configuration
     app.config.from_object('config.Config')
 
-    # Initialize Plugins
     db.init_app(app)
     login_manager.init_app(app)
 
@@ -35,14 +32,11 @@ def create_app():
         from . import auth
         from .assets import compile_assets
 
-        # Register Blueprints
         app.register_blueprint(routes.main_bp)
         app.register_blueprint(auth.auth_bp)
 
-        # Create Database Models
         db.create_all()
 
-        # Compile static assets
         if app.config['FLASK_ENV'] == 'development':
             compile_assets(app)
 
